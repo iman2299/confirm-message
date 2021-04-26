0e84e0a5676c026f89e0d6430187d1b67f37a7a2
iman2299 <imanhasannaser@gmail.com>
2021-04-26 06:13:49 +0300

    first commit

---
+++ confirm massage.js
@@ -0,0 +1,11 @@
+/* write a small web app that does the following:
+    - keeps showing the user a confirm dialog box as long as the user clicks Ok.
+    - when the user clicks Cancel it stops*/
+   // let  myName = "IMAN " ;
+    let name = prompt('What Is Your Name?', );
+    while (myName=confirm) {
+        if (!name) break;  
+    alert(name )
+
+   
+    }
